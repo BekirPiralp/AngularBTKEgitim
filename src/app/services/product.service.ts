446f28548ecc42f80a2bf8bcd8356a4e551905d4
BekirPiralp <bpir33@gmail.com>
2022-07-24 20:39:25 +0300

    gelen path e göre fitreleme yaptık

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable,throwError } from 'rxjs';
-import { tap,catchError} from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
   path = "http://localhost:3000/products";
 
-  getProducts(categoryId:any):Observable<Product[]>{
-    return this.http.get<Product[]>(this.path+"?categoryId="+categoryId).pipe(
-      tap(data => console.log(JSON.stringify(data)) ), //loglama
+  getProducts(categoryId: any): Observable<Product[]> {
+    let newPath = this.path;
+    if (categoryId) {
+      newPath += "?categoryId=" + categoryId
+    }
+
+    return this.http.get<Product[]>(newPath).pipe(
+      tap(data => console.log(JSON.stringify(data))), //loglama
       catchError(this.handleError) // hata ayıklama
     );
   }
 
-  handleError(hata:HttpErrorResponse){
-    let erroroMessage=""
-    if(hata.error instanceof ErrorEvent){
-      erroroMessage = "Bir hata oluştu "+hata.error.message
-    }else{
+  handleError(hata: HttpErrorResponse) {
+    let erroroMessage = ""
+    if (hata.error instanceof ErrorEvent) {
+      erroroMessage = "Bir hata oluştu " + hata.error.message
+    } else {
       erroroMessage = "Sistemsel bir hata"
     }
 
-    return throwError(()=>new Error(erroroMessage));
+    return throwError(() => new Error(erroroMessage));
   }
 }
