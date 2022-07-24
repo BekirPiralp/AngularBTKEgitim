3aaf80823e8a35f0c820e81b3dc69d5f351157cc
BekirPiralp <bpir33@gmail.com>
2022-07-24 19:29:33 +0300

    observable loglama ve hata denetimi yapıldı bunu için pipe kullanıldı

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { Observable,throwError } from 'rxjs';
+import { tap,catchError} from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -10,6 +11,20 @@ export class ProductService {
   path = "http://localhost:3000/products";
 
   getProducts():Observable<Product[]>{
-    return this.http.get<Product[]>(this.path);
+    return this.http.get<Product[]>(this.path).pipe(
+      tap(data => console.log(JSON.stringify(data)) ), //loglama
+      catchError(this.handleError) // hata ayıklama
+    );
+  }
+
+  handleError(hata:HttpErrorResponse){
+    let erroroMessage=""
+    if(hata.error instanceof ErrorEvent){
+      erroroMessage = "Bir hata oluştu "+hata.error.message
+    }else{
+      erroroMessage = "Sistemsel bir hata"
+    }
+
+    return throwError(()=>new Error(erroroMessage));
   }
 }
