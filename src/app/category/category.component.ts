50bda7ccf973b7eede4431c7cad6ebde1df3260e
BekirPiralp <bpir33@gmail.com>
2022-07-24 19:46:34 +0300

    kategori içinde servis çağrıldı ve api den almak için gerekli ayarlar yapıldı.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 import { Category } from './category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService
+  ) { }
+
   title: string = "Kategori Listesi";
-  categories:Category[]=[
-    {id:1,name:"Elektronik"},
-    {id:2,name:"Laptop"},
-    {id:3,name:"Mause"}
-  ];
+  categories!: Category[];
+
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data=>{
+      this.categories = data;
+    });
   }
 
 }
