4a0a31e9f00ec792a49dc905957d722129b9ad46
BekirPiralp <bpir33@gmail.com>
2022-07-28 22:02:47 +0300

    şuanki hali

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
   path = "http://localhost:3000/products";
-
+  //
   getProducts(categoryId: any): Observable<Product[]> {
     let newPath = this.path;
     if (categoryId) {
