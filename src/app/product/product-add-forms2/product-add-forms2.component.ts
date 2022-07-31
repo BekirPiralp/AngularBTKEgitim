2272da127ea8070e061c24a15f733121e58f4113
BekirPiralp <bpir33@gmail.com>
2022-07-31 22:05:22 +0300

    reactive ile form yazıldı ve gerekli ayarlamalar yapıldı birkaç yere ufak ayar yapıldı.

--- src/app/product/product-add-forms2/product-add-forms2.component.ts
+++ src/app/product/product-add-forms2/product-add-forms2.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/category/category';
+import { AlertifyServisiService } from 'src/app/services/alertify-servisi.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-forms2',
   templateUrl: './product-add-forms2.component.html',
-  styleUrls: ['./product-add-forms2.component.css']
+  styleUrls: ['./product-add-forms2.component.css'],
+  providers: [CategoryService,ProductService]
 })
 export class ProductAddForms2Component implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder, 
+    private categoryService: CategoryService,
+    private productService:ProductService,
+    private alertfyService:AlertifyServisiService) { }
 
-  ngOnInit(): void {
+  productAddForm!: FormGroup;
+
+  product!: Product;
+  categories!: Category[];
+
+  createProductAddFrom() {
+    this.productAddForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      description: ["", Validators.required],
+      imageUrl: ["", Validators.required],
+      price: ["", Validators.required],
+      categoryId: ["", Validators.required]
+    });
   }
 
+
+  ngOnInit(): void {
+    this.createProductAddFrom();
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data
+    }
+    )
+  }
+
+  add() {
+    if (this.productAddForm.valid) {
+      this.product = Object.assign({}, this.productAddForm.value)
+    }
+
+    this.productService.addProduct(this.product).subscribe(data=>{
+      this.alertfyService.basarili(data.name+" başarıyla eklendi");
+    })
+  }
 }
